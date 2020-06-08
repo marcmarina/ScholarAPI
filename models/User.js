e4d2bf88bc4bdc9ffa583ab7db2c4e84b1b7b608
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2020-06-08 20:41:22 +0200

    Added User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    emailVerifiedAt: {
+      type: Date,
+    },
+    subjects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Subject",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
