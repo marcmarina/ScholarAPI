e0ef04a3711608ea98c43b3543d49618bf478360
Marc Marina <marc.marina.miravitlles@gmail.com>
2020-06-10 11:27:09 +0200

    Add session working, locale needs fixing

--- controllers/studySession.js
+++ controllers/studySession.js
@@ -1,7 +1,8 @@
 const { validationResult } = require("express-validator");
+const moment = require("moment");
+
 const StudySession = require("../models/StudySession");
 const Subject = require("../models/Subject");
-
 const ErrorHandler = require("../util/error-handler");
 
 // Get all the subjects for the given subject
@@ -26,7 +27,28 @@ exports.index = async (req, res, next) => {
 
 exports.create = async (req, res, next) => {
   const errorHandler = new ErrorHandler(validationResult(req));
-
   if (errorHandler.errors.size > 0)
     return res.status(422).json({ errors: errorHandler.getErrors() });
+
+  const subjectId = req.body.subjectId;
+  const duration = req.body.duration;
+  const date = moment(req.body.date).toDate();
+
+  try {
+    const subject = await Subject.findById(subjectId);
+    if (!subject)
+      res.status(404).json({ errors: { subject: ["Subject not found."] } });
+    const session = new StudySession({
+      duration: duration,
+      date: date,
+      subject: subjectId,
+    });
+    const result = await session.save();
+    subject.studySessions.push(result._id);
+    await subject.save();
+    res.status(201).json();
+  } catch (err) {
+    if (!err.statusCode) err.statusCode = 500;
+    next(err);
+  }
 };
--- routes/studySession.js
+++ routes/studySession.js
@@ -10,4 +10,19 @@ const isAuth = require("../middleware/is-auth");
 // Returns all sessions for a given subject
 router.get("/studySessions/:subjectId", isAuth, studySessionController.index);
 
+router.post(
+  "/studySessions",
+  isAuth,
+  [
+    check("duration")
+      .isNumeric()
+      .withMessage("The duration has to be a number."),
+    check("date").custom((value, { req }) => {
+      if (!Date.parse(value)) throw new Error("The date is not valid.");
+      return true;
+    }),
+  ],
+  studySessionController.create
+);
+
 module.exports = router;
