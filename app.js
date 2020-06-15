be5361cbc386a817bcff152ac6d4a75378ed7bfa
Marc Marina <marc.marina.miravitlles@gmail.com>
2020-06-15 21:00:44 +0200

    Now using CORS package to handle requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,15 +10,7 @@ const env = require("./util/env");
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, DELETE, PUT, PATCH"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
 
 app.get("/", (req, res, next) => {
   let response = require("./package.json");
