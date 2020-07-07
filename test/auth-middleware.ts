5e95acf663c37f79ce20650ac224c2f8ceb17dd3
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2020-07-07 23:50:55 +0200

    Added tests

---
+++ test/auth-middleware.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import jsonwebtoken from 'jsonwebtoken';
+import sinon from 'sinon';
+
+import authMiddleware from '../src/middleware/is-auth';
+
+describe('Auth Middleware', () => {
+  it('gives error if no authorization header is present', function () {
+    const req = {
+      get: function () {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not authenticated.'
+    );
+  });
+
+  it('should throw error if authorization header is only one string', function () {
+    const req = {
+      get: function () {
+        return 'xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should throw an error if the token cannot be verified', function () {
+    const req = {
+      get: function () {
+        return 'Bearer xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  // it('should yield a userId after decoding the token', function () {
+  //   const req = {
+  //     get: function () {
+  //       return 'Bearer xyz';
+  //     },
+  //   };
+  //   sinon.stub(jsonwebtoken, 'verify');
+  //   jsonwebtoken.verify.returns({ userId: 'abc' });
+  //   authMiddleware(req, {}, () => {});
+  //   expect(req).to.have.property('userId');
+  //   jsonwebtoken.verify.restore();
+  // });
+});
