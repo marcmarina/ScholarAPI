afe32f20b441a35545c5a818cbbe9940ef14a484
Marc Marina <marc.marina.miravitlles@gmail.com>
2020-07-07 15:58:46 +0200

    CD Working

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.get('/', (req, res, next) => {
-//   const response = require('../package.json');
-//   delete response['repository'];
-//   res.status(200).json(response);
-// });
-
 app.get('/', (req, res, next) => {
-  res.status(200).json({ deploy: 'working' });
+  const response = require('../package.json');
+  delete response['repository'];
+  res.status(200).json(response);
 });
 
 app.use(UserRoutes);
