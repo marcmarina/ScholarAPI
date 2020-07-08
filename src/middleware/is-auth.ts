4cbe6ecf6de0d37dea49d0baf5bc761cbb6957bd
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2020-07-08 12:24:01 +0200

    Auth middleware

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -1,12 +1,12 @@
 import jwt from 'jsonwebtoken';
 
 import { getSecretKey } from '../utils/env';
+import CustomError from '../utils/CustomError';
 
 export default (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    const error = new Error('Not authenticated.');
-    // error.statusCode = 401;
+    const error = new CustomError('Not authenticated.', 401);
     throw error;
   }
   const token = authHeader.split(' ')[1];
@@ -14,12 +14,11 @@ export default (req, res, next) => {
   try {
     decodedToken = jwt.verify(token, getSecretKey());
   } catch (err) {
-    // err.statusCode = 500;
+    err.statusCode = 500;
     throw err;
   }
   if (!decodedToken) {
-    const error = new Error('Not authenticated.');
-    // error.statusCode = 401;
+    const error = new CustomError('Not authenticated.', 401);
     throw error;
   }
   req.userId = decodedToken.userId;
